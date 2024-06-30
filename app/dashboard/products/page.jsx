2519f22dfc660225bc1aac6283b0a8713c0f7f27
vimansha <vimanshamadushani88@gmail.com>
2024-06-30 14:51:06 +0530

    feat: users & product pages design

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,9 +1,108 @@
 import React from "react";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
 const ProductPage = () => {
   return (
-    <div>
-      <p>Product page</p>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Created At</td>
+            <td>Price</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                IPhone
+              </div>
+            </td>
+
+            <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</td>
+            <td>30.06.2024</td>
+            <td>$999</td>
+            <td>72</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                IPhone
+              </div>
+            </td>
+
+            <td>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi,
+              eius.
+            </td>
+            <td>30.06.2024</td>
+            <td>$999</td>
+            <td>72</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <Pagination />
     </div>
   );
 };
