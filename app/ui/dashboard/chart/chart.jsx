8cddd37ca2db30db6eb12eaf1f10f0d26accdf36
vimansha <vimanshamadushani88@gmail.com>
2024-06-30 11:46:11 +0530

    feat: cards and transaction design

---
+++ app/ui/dashboard/chart/chart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./chart.module.css";
+
+const Chart = () => {
+  return (
+    <div className={styles.container}>
+      <p>Chart</p>
+    </div>
+  );
+};
+
+export default Chart;
---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./rightbar.module.css";
+
+const Rightbar = () => {
+  return (
+    <div className={styles.container}>
+      <p>Rightbar</p>
+    </div>
+  );
+};
+
+export default Rightbar;
