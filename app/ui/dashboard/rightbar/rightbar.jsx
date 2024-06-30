c51748cce896ec61f5f9cd339400d4e4af63e747
vimansha <vimanshamadushani88@gmail.com>
2024-06-30 13:20:29 +0530

    feat: rightbar design

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,10 +1,53 @@
 import React from "react";
 import styles from "./rightbar.module.css";
+import Image from "next/image";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
 
 const Rightbar = () => {
   return (
     <div className={styles.container}>
-      <p>Rightbar</p>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+        </div>
+
+        <div className={styles.texts}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            deleniti! Totam corrupti excepturi eos ut.
+          </p>
+
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions are available, partial pre-rendering is coming
+            up!
+          </h3>
+          <span className={styles.subtitle}>Boost your productivity</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            deleniti! Totam corrupti excepturi eos ut.
+          </p>
+
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
